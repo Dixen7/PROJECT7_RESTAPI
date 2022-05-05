51f58726d7fc23770dabfb74bd869f639c0713c5
Dixen7 <dixenlive@gmail.com>
2022-05-05 17:15:55 +0200

    02_iplement_user_controller_manager

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/UserDetailsServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/UserDetailsServiceImpl.java
@@ -1,11 +1,12 @@
 package com.nnk.springboot.configuration;
 
-import com.nnk.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.nnk.springboot.repositories.UserRepository;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -16,8 +17,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
-                .orElseThrow(() ->
-                { return new UsernameNotFoundException("Username " + username + " not found");});
+                .orElseThrow(() -> {
+                    return new UsernameNotFoundException(
+                            "Username " + username + " not found");
+                });
     }
 
 }
